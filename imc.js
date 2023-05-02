6bb767e3745478474ecf1e75f6ab5cc05e9e0954
Isabila2 <125681801+Isabila2@users.noreply.github.com>
2023-05-02 20:21:31 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,16 @@
+var user = ["Isabella","Marcio","Calu"];
+var senha = ["1234","5678","9101"];
+var verificar;
+
+function login(){
+	verificar = true;
+	if(usuario == user[0]){
+	if (senha == senha[0]{
+		window.alert("Ir para HOME");
+		verificar = false;
+	}
+	}
+	while (verificar == "true");
+}
+		
+}
